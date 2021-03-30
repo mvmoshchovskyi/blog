a3a78317d573992df43d852c5fa2a6a28ccffb3a
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2021-03-30 16:49:36 +0300

    v2 create user feature module , with userService and userController userEntity userInterfase add postman request

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      is: true,
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -14,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
